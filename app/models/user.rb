900be65b95163f47e9193ee871649472ba1fc9e8
Olivier Girardot <olive_girardot@hotmail.com>
2020-02-25 12:20:06 +0100

    solved the sign in problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :full_name, uniqueness: true
-  validates :full_name, presence: true
+  # validates :full_name, uniqueness: true
+  # validates :full_name, presence: true
 end
