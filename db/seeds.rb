3310d06cb78707ab8bd3b55b47049b2b9fe8eec5
Leoni Sperling <leoni.sperling@gmail.com>
2020-02-26 14:22:34 +0100

    seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+casper = User.create(full_name: 'Casper Ghost')
+
 puts "generating hauted houses"
-Property.create(name: "Bates Motel", address: "old highway 10" ,price: 200, haunted_level: 5, user_id: 1)
-Property.create(name: "Ghost Cottage", address: "old highway 10" ,price: 200, haunted_level: 4, user_id: 1)
-Property.create(name: "The creeps", address: "Zittauer Str. 63-55, 12355 Berlin" ,price: 200, haunted_level: 5, user_id: 1)
-Property.create(name: "Haunted hotel", address: " Birkbuschstraße 36, 12167 Berlin" ,price: 200, haunted_level: 2, user_id: 1)
-Property.create(name: "Scary Mansion", address: "18 Rue de Paradis, 75010 Paris, France",price: 200, haunted_level: 1, user_id: 1)
+Property.create(name: "Bates Motel", address: "old highway 10" ,price: 200, haunted_level: 5, user_id: casper.id)
+Property.create(name: "Ghost Cottage", address: "old highway 10" ,price: 200, haunted_level: 4, user_id: casper.id)
+Property.create(name: "The creeps", address: "Zittauer Str. 63-55, 12355 Berlin" ,price: 200, haunted_level: 5, user_id: casper.id)
+Property.create(name: "Haunted hotel", address: " Birkbuschstraße 36, 12167 Berlin" ,price: 200, haunted_level: 2, user_id: casper.id)
+Property.create(name: "Scary Mansion", address: "18 Rue de Paradis, 75010 Paris, France",price: 200, haunted_level: 1, user_id: casper.id)
 puts "hauted houses are ready!👻"
+
