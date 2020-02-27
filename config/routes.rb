e76789d71c1b280472cbba4ddc87367b061720e9
sean fu <sean14xx@gmail.com>
2020-02-27 14:45:59 +0100

    create dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create] do
     resources :bookings, only: [:new, :create]
     resources :reviews, only: [:new, :create]
+    resources :dashboard, only: [:index, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
