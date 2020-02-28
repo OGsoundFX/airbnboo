7efe29f104d109246fbced384831786f78c7b0fa
Olivier Girardot <olive_girardot@hotmail.com>
2020-02-28 13:47:27 +0100

    made the review form a little better

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :booking
-  validates :rating, scope
+  validates :rating, inclusion: { in: (1..5) }
   validates :content, length: { minimum: 15 }
 end
